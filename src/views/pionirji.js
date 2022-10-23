c0d14fb5a7d6d2a0fb43d08de61d28b72e3ebeb9
juremohar <jure.mohar5@gmail.com>
2022-10-23 20:31:57 +0200

    pionirji & mladinci history questions

--- src/views/pionirji.js
+++ src/views/pionirji.js
@@ -2,13 +2,12 @@
 var m = require("mithril")
 const trueFalseJson = require('../../data/ves_neves.json').filter(q => q.ages.includes("pioner"));
 const firstAidJson = require('../../data/prva_pomoc.json').filter(q => q.ages.includes("pioner"));
-const firePreventionJson = require('../../data/pozarna_preventiva.json').filter(q => q.ages.includes("pioner"));
+const historyJson = require('../../data/zgodovina.json').filter(q => q.ages.includes("pioner"));
 
 function loadQuestions() {
     return {
         "trueFalse": getQuestions(trueFalseJson, 10),
-        "firstAid": getQuestions(firstAidJson, 10),
-        "firePrevention": getQuestions(firePreventionJson, 15)
+        "multipleChoice": getQuestions([...firstAidJson, ...historyJson], 20)
     }
 }
 
@@ -31,19 +30,7 @@ function generateTrueFalseQuestions(questions) {
     });
 }
 
-function generateFirstAidQuestions(questions) {
-    return questions.map(q => {
-        let answears = q.wrongAnswears.sort(function() {return 0.5 - Math.random()}).slice(0, 2);
-        answears.push(q.correctAnswear)
-
-        return {
-            question: q.question,
-            answears: answears.sort(function() {return 0.5 - Math.random()})
-        };
-    });
-}
-
-function generateFirePreventiondQuestions(questions) {
+function generateMultipleOptionsQuestions(questions) {
     return questions.map(q => {
         let answears = q.wrongAnswears.sort(function() {return 0.5 - Math.random()}).slice(0, 2);
         answears.push(q.correctAnswear)
@@ -83,10 +70,10 @@ let trueFalseQuestions = {
     }
 }
 
-let firstAidQuestions = {
+let multipleChoiceQuestions = {
     questions: null,
     oninit: function(vnode) {
-        this.questions = generateFirstAidQuestions(vnode.attrs.questions)
+        this.questions = generateMultipleOptionsQuestions(vnode.attrs.questions)
     },
     view: function() {
         if (!this.questions) {
@@ -94,8 +81,8 @@ let firstAidQuestions = {
         }
 
         return m("div", [
-            m("div.fs-4.fw-bold", "Prva pomoč"),
-            m("div.mb-2.fs-7.fw-light", "Spodaj je 10 vprašanj. Izberite kateri odgovor je pravilen."),
+            m("div.fs-4.fw-bold", "Prva pomoč, zgodovina in požarna preventiva"),
+            m("div.mb-2.fs-7.fw-light", "Spodaj je 20 vprašanj. Izberite kateri odgovor je pravilen."),
             [
                 this.questions.map((q, index) => {
                     return m("div.mb-4", [
@@ -112,38 +99,6 @@ let firstAidQuestions = {
         ])
     }
 }
-
-let firePreventionQuestions = {
-    questions: null,
-    oninit: function(vnode) {
-        this.questions = generateFirePreventiondQuestions(vnode.attrs.questions)
-    },
-    view: function() {
-        if (!this.questions) {
-            return m("div", "Generiram vprašanja ...")
-        }
-
-        return m("div", [
-            m("div.fs-4.fw-bold", "Požarna preventiva"),
-            m("div.mb-2.fs-7.fw-light", "Spodaj je 10 vprašanj. Izberite kateri odgovor je pravilen."),
-            [
-                this.questions.map((q, index) => {
-                    return m("div.mb-4", [
-                        m("div", [
-                            m("span.fw-bold", `Vprašanje ${index + 1}: `),
-                            q.question
-                        ]),
-                        m("ol.ps-4", { type: "a" } ,q.answears.map(answear => {
-                            return m("li.ps-2", answear)
-                        }))
-                    ])
-                })
-            ]
-        ])
-    }
-}
-
-
 
 module.exports = {
     questions: null,
@@ -157,8 +112,7 @@ module.exports = {
 
         return m("div.container", [
             m(trueFalseQuestions, {questions: this.questions.trueFalse}),
-            m(firstAidQuestions, {questions: this.questions.firstAid}),
-            m(firePreventionQuestions, {questions: this.questions.firePrevention})
+            m(multipleChoiceQuestions, {questions: this.questions.multipleChoice})
         ])
     }
 }
