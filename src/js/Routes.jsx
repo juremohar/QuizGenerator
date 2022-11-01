51ddf1a3375e3e746de762970c3dadc95eb07bf4
juremohar <jure.mohar5@gmail.com>
2022-11-01 17:16:40 +0100

    Add home page

--- src/js/Routes.jsx
+++ src/js/Routes.jsx
@@ -2,19 +2,27 @@ import m from 'mithril';
 
 import { PionirjiView } from './views/Pionirji';
 import { MainLayout } from './layouts/MainLayout';
+import { HomeView } from './views/Home';
 
 function initRouter(el) {
-    m.route.prefix = '';
+    // m.route.prefix = '';
 
     m.route(el, '/', {
         '/': {
+            render: function() {
+                return <MainLayout>
+                    <HomeView />
+                </MainLayout>
+            }
+        },
+        '/pionirji': {
             render: function() {
                 return <MainLayout>
                     <PionirjiView />
                 </MainLayout>
             }
         },
-        '/home': {
+        '/mladinci': {
             render: function() {
                 return <MainLayout>
                     <PionirjiView />
