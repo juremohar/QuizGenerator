7e4738977ff389adeb8e769037c2c923b6dabdac
juremohar <jure.mohar5@gmail.com>
2022-11-02 17:47:25 +0100

    Seperate index html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -60,6 +61,10 @@ module.exports = {
   plugins: [
     new webpack.ProvidePlugin({
         m: "mithril",
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Gasilski kviz',
+      template: 'index.html'
     }),
   ]
 };
