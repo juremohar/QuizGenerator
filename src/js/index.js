d383cbb43bdb8fa9a75b058c0065616a90c0a40e
juremohar <jure.mohar5@gmail.com>
2022-11-14 20:06:59 +0100

    Add analytics

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
 import { initRouter } from './Routes';
 import '../scss/styles.scss'
+import { inject } from '@vercel/analytics';
 
 window.addEventListener('DOMContentLoaded', function() {
   initRouter(document.body)
+  inject();
 })
